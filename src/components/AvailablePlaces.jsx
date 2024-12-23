4df566c2605bf70ce85071bf47a89ce9404c9cbf
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-23 21:02:32 +0700

    How not to send HTTP Requests (and why it's wrong)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,8 +6,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   //Fetching HTTP Request
+  fetch('http://localhost:3000/places').then((response) => {
+    return response.json();
+  }).then((resData) => {
+    setAvailablePlaces(resData.places);
+  }) //This is not correct, it will cause infinite loop.
 
-  
   return (
     <Places
       title="Available Places"

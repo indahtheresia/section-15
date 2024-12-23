2eeb4e6c26022a5d900d2627056f3ecc3b620ebf
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-23 21:44:00 +0700

    Using async / await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,12 +6,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   //Fetching HTTP Request
+  // First solution
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/places').then((response) => {
+  //     return response.json();
+  //   }).then((resData) => {
+  //     setAvailablePlaces(resData.places);
+  //   }); 
+  // }, []);
+
+  // Second solution / Alternative way
   useEffect(() => {
-    fetch('http://localhost:3000/places').then((response) => {
-      return response.json();
-    }).then((resData) => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
       setAvailablePlaces(resData.places);
-    }); 
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
